e6bf613de0e62df9e942c207f9ae24915c6757ee
tlantea23 <tlantea2306@gmail.com>
2026-06-01 22:53:37 +0530

    Update route.js

--- app/api/chat/route.js
+++ app/api/chat/route.js
@@ -15,7 +15,7 @@ export async function POST(request) {
         'Content-Type': 'application/json'
       },
       body: JSON.stringify({
-        model: 'llama-3.3-70b-versatile',
+        model:'meta-llama/llama-4-scout-17b-16e-instruct',
         messages: [
           {
             role: 'system',
